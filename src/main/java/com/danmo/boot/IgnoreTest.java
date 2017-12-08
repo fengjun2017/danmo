286cd8880725ce9ab5c48cf8782353b1da59d381
598455803@qq.copm <598455803@qq.copm>
2017-12-09 00:46:59 +0800

    dev

--- src/main/java/com/danmo/boot/IgnoreTest.java
+++ src/main/java/com/danmo/boot/IgnoreTest.java
@@ -3,6 +3,6 @@ package com.danmo.boot;
 public class IgnoreTest {
 
     public static void main(String [] args){
-        System.out.println("danmo dev 明天鹿姐请吃烧烤");
+        System.out.println("danmo prod 1111");
     }
 }
