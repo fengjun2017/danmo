f0774837717451a378279b3dc89051205a172aaf
598455803@qq.copm <598455803@qq.copm>
2017-12-09 00:34:04 +0800

    dev

--- src/main/java/com/danmo/boot/IgnoreTest.java
+++ src/main/java/com/danmo/boot/IgnoreTest.java
@@ -3,6 +3,6 @@ package com.danmo.boot;
 public class IgnoreTest {
 
     public static void main(String [] args){
-        System.out.println("danmo");
+        System.out.println("danmo dev");
     }
 }
